f9e9234a485c76fa07d646d8992095695a7acb93
Manuel Gonzalez <manuel.gonzalez.z@ug.uchile.cl>
2020-08-09 20:27:14 -0400

    Create pi_nth.py
    Chudnovsky algorithm for pi to the nth digit

---
+++ pi_nth.py
@@ -0,0 +1,21 @@
+#This programs calculates pi using the Chudnovsky algorithm to the nth digit enter by the user
+import numpy as np 
+from decimal import Decimal, getcontext
+
+print("I'll calculate pi to the nth number, please enter n")
+n=int(input())
+getcontext().prec=n
+C = Decimal(426880*np.sqrt(10005))
+L=13591409
+M=1
+X=1
+K=6
+suma=13591409
+for i in range(1,100):
+    M *= Decimal((K**3-16*K)/(K+1)**3)
+    L += 545140134
+    X *= -262537412640768000
+    suma +=  M*L/X
+    K += 12
+pi= C/suma
+print("pi to the ",n ,"th digit is ", pi)
